a423ba3a4c61466dabfc255679da4b46706f53ee
ericcchiu <eric.chn.chiu@gmail.com>
2018-06-11 12:34:27 -0500

    Added isGreaterThanTen function

---
+++ ChallengeProbs/012_isGreaterThanTen.js
@@ -0,0 +1,19 @@
+/* Write a function called “isGreaterThan10”.
+Given a number, “isGreaterThan10” returns whether the given number is greater than 10.
+
+var output = isGreaterThan10(11);
+console.log(output); // --> true
+*/
+
+function isGreaterThan10 (number) {
+  return (number>10) ? true: false ; 
+}
+
+var output = isGreaterThan10(11);
+console.log(output); // --> true
+
+var output1 = isGreaterThan10(10);
+console.log(output1); // --> true
+
+
+
