b6292ca5c7afbcb96217d60fb6c07768b10d9e81
ericcchiu <eric.chn.chiu@gmail.com>
2018-06-11 11:15:48 -0500

    Added isOldEnoughToDrinkAndDrive.js

---
+++ ChallengeProbs/004_isOldEnoughToDrinkAndDrive.js
@@ -0,0 +1,18 @@
+/* Write a function called “isOldEnoughToDrinkAndDrive”.
+Given a number, in this case an age, “isOldEnoughToDrinkAndDrive” returns whether a person of this given age is old enough to legally drink and drive in the United States.
+Notes:
+* The legal drinking age in the United States is 21.
+* It is always illegal to drink and drive in the United States. */
+
+function isOldEnoughToDrinkAndDrive (age) { 
+  return false; 
+}
+
+// Input testing 
+var age = 16; 
+var age1 = 21; 
+var age2 = 22; 
+
+console.log(isOldEnoughToDrinkAndDrive(age)); 
+console.log(isOldEnoughToDrinkAndDrive(age1)); 
+console.log(isOldEnoughToDrinkAndDrive(age2)); 
