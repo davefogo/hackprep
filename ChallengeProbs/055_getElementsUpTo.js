dc6357b28a8db7e54a4ddc88be0a129a0dcd6434
ericcchiu <eric.chn.chiu@gmail.com>
2018-06-15 14:30:07 -0500

    Added getElementsUpTo function

---
+++ ChallengeProbs/055_getElementsUpTo.js
@@ -0,0 +1,20 @@
+/* 
+Write a function called “getElementsUpTo”.
+
+Given an array and a index, “getElementsUpTo”, returns an array with all the elements up until, but not including, the element at the given index.
+
+Notes:
+* In order to do this you should be familiar with the ‘slice’ method.
+
+var output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
+console.log(output); // --> ['a', 'b', 'c']
+*/ 
+
+// Main function 
+var getElementsUpTo= function (array, index) { 
+  return array.slice(0,index); 
+}
+
+// Input test
+var output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
+console.log(output); // --> ['a', 'b', 'c']
